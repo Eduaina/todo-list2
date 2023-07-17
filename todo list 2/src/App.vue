89b398682b6992fbbcc517c818bf3fa56234dce7
Eduaina <eduaighalo29@gmail.com>
2023-07-17 20:30:29 +0100

    A Simple todo list

---
+++ todo list 2/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+  <section class="section">
+    <header class="header">
+      <h1>A Simple NoteApp</h1>
+      <span>NOTEPAD</span>
+      <button>gray</button>
+
+    </header>
+
+    <div>
+
+    </div>
+  </section>
+
+</template>
+
+<style scoped>
+
+</style>
